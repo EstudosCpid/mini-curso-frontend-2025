7e5a13309f8705f1a9c6f9f4ee142d29343a5c27
guirm <guilherme_rm@yahoo.com.br>
2025-04-17 12:11:44 -0300

    Boa sorte, pequeno gafanhoto!

---
+++ docs-mini-curso/MainLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+      <q-header elevated>
+        <q-toolbar>
+          <q-toobar-title> Quasar APP - CRUD Simples</q-toobar-title>
+        </q-toolbar>
+      </q-header>
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </q-layout>
+  </template>
